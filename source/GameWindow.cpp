2c16d07a421b122aea7fda1049e91bae537988d0
quyykk <quyykk@protonmail.com>
2021-09-01 18:27:54 +0200

    Choose glsl version dynamically.

--- source/GameWindow.cpp
+++ source/GameWindow.cpp
@@ -233,7 +233,7 @@ bool GameWindow::Init()
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
 	ImGui_ImplSDL2_InitForOpenGL(mainWindow, context);
-	ImGui_ImplOpenGL3_Init("#version 460");
+	ImGui_ImplOpenGL3_Init(nullptr);
 	
 	return true;
 }
